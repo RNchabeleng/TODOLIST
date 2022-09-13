a6b26569dbe15ca79e6dcbb47db18850a1014f86
RNchabeleng <rikinchabeleng@gmail.com>
2022-09-13 18:43:01 +0200

    Updating JS
    Correcting JS link, setting up the labels I need, changing innerHTML to readonly,  appending elements, toggling Delete and Edit button,

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 window.addEventListener('load', () => {
     todos = JSON.parse(localStorage.getItem('todos')) || [];
-    const nameInput = document.querySelector("#name"); 
+    const nameInput = document.querySelector("#name");
     const newTodoForm = document.querySelector('#new.todo-form');
-    
+
     const username = localStorage.getItem('username') || '';
 
-   nameInput.value = username;
+    nameInput.value = username;
 
-   nameInput.addEventListener('change', e => {
-    localStorage.setItem('username', e.target.value);
-   })
+    nameInput.addEventListener('change', e => {
+        localStorage.setItem('username', e.target.value);
+    })
 
-newTodoForm.addEventListener('submit', e => {
-    e.preventDefault();
+    newTodoForm.addEventListener('submit', e => {
+        e.preventDefault();
 
-    const todo ={
-        content: e.target.elements.content.value,
-        category: e.target.elements.category.value,
-        done: false,
-        createdAt: new Date().getTime()
-    }
+        const todo = {
+            content: e.target.elements.content.value,
+            category: e.target.elements.category.value,
+            done: false,
+            createdAt: new Date().getTime()
+        }
 
-    todos.push(todo);
+        todos.push(todo);
 
-    localStorage.setItem('todos', JSON.stringify(todo-list));
+        localStorage.setItem('todos', JSON.stringify(todo - list));
 
-    e.target.reset();
+        e.target.reset();
+
+        DisplayTodos();
+    })
 
     DisplayTodos();
 })
 
-DisplayTodos();
-})
-
-function DisplayTodos () {
+function DisplayTodos() {
     const todoList = document.querySelector('#todo-list');
 
     todoList.innerHTML = '';
@@ -62,6 +62,57 @@ function DisplayTodos () {
 
         content.classList.add('todo-content');
         actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteButton.classList.add('delete');
 
+        content.innerHTML = `<input type ="text" value="${todo.content}"
+    readonly>`;
+        edit.innerHTML = "Edit";
+        deleteButton.innerHTML = "Delete";
+
+        label.appendChild(input);
+        label.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deleteButton);
+        todoItem.appendChild(label);
+        todoItem.appendChild(actions);
+
+        todoList.appendChild(todoItem);
+
+        if (todo.done) {
+            todoItem.classList.add('done');
+        }
+
+        input.addEventListener("click", e => {
+            todo.done = e.target.checked;
+            localStorage.setItem('todos', JSON.stringify(todos));
+
+
+            if (todo.done) {
+                todoItem.classList.add('done');
+            } else {
+                todoItem.classList.remove('done');
+            }
+
+            DisplayTodos();
+        })
+
+        edit.addEventListener('click', e => {
+            const input = content.querySelector('input');
+            input.removeAttribute('readonly');
+            input.focus();
+            input.addEventListener('blur', e => {
+                input.setAttribute('readonly', true);
+                todo.content = e.target.value;
+                localStorage.setItem('todos', JSON.stringify(todos));
+                DisplayTodos();
+            })
+        })
+
+        deleteButton.addEventListener('click', e => {
+            todos = todos.filter(t => t != todo);
+            localStorage.setItem('todo', JSON.stringify(todos));
+            DisplayTodos();
+        })
     })
 }
